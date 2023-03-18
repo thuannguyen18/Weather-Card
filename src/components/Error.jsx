76abada0baebc26726ea04081bd69c6975d26ab2
Thuan <nguyenmthuan18@gmail.com>
2023-03-18 23:04:42 +0700

    second commit

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 function Error() {
     return (
         <div className="error-container">
-            <img className="error-img" src="https://static.vecteezy.com/system/resources/thumbnails/008/255/803/small/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-a-hand-drawn-layout-template-of-a-broken-robot-illustration-vector.jpg" alt="Not Found" />
+            <img className="error-img" src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg" alt="Not Found" />
         </div>
     );
 }
